384a0855f0c5d06309df7eed4eab8e9d8af5bda9
dwg241 <57422055+dwg241@users.noreply.github.com>
2020-03-20 17:32:15 +0900

    Delete DBsearch.py

--- final/DBsearch.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-con = sqlite3.connect('DB_25.db')
-cursor = con.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-#cursor.execute("select*FROM test_1 ")
-
-print(cursor.fetchall())
-
-
