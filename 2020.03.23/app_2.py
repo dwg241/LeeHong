44fee47156451d480fe93db5fbfb3172f6d4ca22
dwg241 <57422055+dwg241@users.noreply.github.com>
2020-03-23 17:51:17 +0900

    Add files via upload

---
+++ 2020.03.23/app_2.py
@@ -0,0 +1,39 @@
+import time
+import sqlite3
+import dht11 as dht
+from flask import Flask , jsonify,render_template
+import hat_test17
+
+#count = 0
+con = sqlite3.connect('DB_28.db')
+cur = con.cursor()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  #  global count
+  #  count += 1
+    return render_template("home3.html",test=hat_test17.gethum())
+@app.route('/dht_db')
+def dht_db():
+    
+	conn = sqlite3.connect('DB_28.db')
+    
+
+	conn.row_factory=sqlite3.Row
+    
+
+	c = conn.cursor()
+
+
+	c.execute("select * from test_1")
+
+	rows = c.fetchall()
+    
+
+	return render_template("dht_db.html",rows = rows,test1=hat_test17.gethum())
+
+
+
+if __name__=='__main__':
+    app.run(host='192.168.0.68',port='5000')
---
+++ 2020.03.23/hat_test17.py
@@ -0,0 +1,32 @@
+import time
+import Adafruit_DHT
+import sqlite3
+
+
+
+def gethum():
+    sensor = Adafruit_DHT.DHT11
+    pin = 5
+    h, t = Adafruit_DHT.read_retry(sensor, pin)
+    result = ''
+   
+    while True:
+    
+        if h is not None and t is not None :
+            con = sqlite3.connect('DB_28.db')
+            cur = con.cursor()
+            sql_insert = "insert into test_1 values('{}', '{}')".format(t,h)
+            cur.execute(sql_insert)
+            con.commit()
+            time.sleep(2)
+
+      
+            result="{},{}".format(t, h)
+
+        else :
+
+            result='Read error'
+    
+        return result
+
+    
