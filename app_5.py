fc2dd1f393e18befdba3effb381d0da14a4b5ab3
dwg241 <57422055+dwg241@users.noreply.github.com>
2020-03-24 12:23:20 +0900

    Add files via upload

--- app_5.py
+++ app_5.py
@@ -1,13 +1,26 @@
+import time
+import sqlite3
 from flask import Flask,render_template,request
-
 app= Flask(__name__)
 
 
-
 @app.route('/')
-def student():
-    f = open('DB_3.txt', 'r')
-    return render_template('test.html',dataone="\n",datatwo="\n".join(f.readlines())+'\n')
+def ftest_1():
 
+	conn = sqlite3.connect('DB_31.db')
+
+	#conn.row_factory=sqlite3.Row
+
+	c = conn.cursor()
+
+	c.execute("select * from test_1")
+
+	rows = c.fetchall()
+    
+
+	return render_template("ftest_1.html",rows = rows)
+@app.route("/graph") 
+def graph(): 
+    return render_template('graph.html') 
 if __name__=='__main__':
     app.run(host='192.168.0.31',port='5000')
