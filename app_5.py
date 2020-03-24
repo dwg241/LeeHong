3a174e3498ffdeb3b8cad4a504beef3e537438a4
dwg241 <57422055+dwg241@users.noreply.github.com>
2020-03-24 14:52:46 +0900

    Add files via upload

--- app_5.py
+++ app_5.py
@@ -1,10 +1,22 @@
 import time
 import sqlite3
-from flask import Flask,render_template,request
+import json
+from flask import Flask,render_template,request,Markup
 app= Flask(__name__)
 
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
 
-@app.route('/')
+
+@app.route('/data.lee')
 def ftest_1():
 
 	conn = sqlite3.connect('DB_31.db')
@@ -18,9 +30,16 @@ def ftest_1():
 	rows = c.fetchall()
     
 
-	return render_template("ftest_1.html",rows = rows)
+	return json.dumps(rows)
 @app.route("/graph") 
 def graph(): 
-    return render_template('graph.html') 
+    
+    conn = sqlite3.connect('DB_33.db')
+
+	#conn.row_factory=sqlite3.Row
+    c = conn.cursor()
+    c.execute("select * from test_1")
+    rows = c.fetchall()
+    return render_template('graph_2.html',max=30, labels=rows, values=rows) 
 if __name__=='__main__':
     app.run(host='192.168.0.31',port='5000')
